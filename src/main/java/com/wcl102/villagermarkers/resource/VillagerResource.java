a336dc1fed08fcb91b50893e548ffd4d5b591c60
delke <delkerbailey@gmail.com>
2022-05-07 00:29:58 -0700

    Defaults to Default marker when a marker isn't found

--- src/main/java/com/wcl102/villagermarkers/resource/VillagerResource.java
+++ src/main/java/com/wcl102/villagermarkers/resource/VillagerResource.java
@@ -1,14 +1,18 @@
 package com.wcl102.villagermarkers.resource;
 
 import com.wcl102.villagermarkers.VillagerMarkersConfig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class VillagerResource {
     public static final Map<String, MarkerResource> resourceCache = new HashMap<>();
+
+    public static final ResourceLocation DEFAULT_ICON = new ResourceLocation("textures/entity/villager/default.png");
 
     private final String careerName;
     private final int level;
@@ -18,24 +22,35 @@ public class VillagerResource {
     public VillagerResource(NBTTagCompound tag) {
         this.careerName = tag.getString("CareerName");
         this.level = tag.getInteger("CareerLevel");
+        this.marker = w();
     }
 
     public MarkerResource getMarker() {
+        return marker;
+    }
+
+    private MarkerResource w() {
         String resourceKey = String.format("%s-%d", this.careerName, this.level);
 
-        // Returned the cached value, if there is one.
         if (resourceCache.containsKey(resourceKey)) {
             return resourceCache.get(resourceKey);
         }
 
-        MarkerResource marker;
         MarkerResource.OverlayType overlayType = MarkerResource.OverlayType.fromValue(VillagerMarkersConfig.overlayIndex).orElse(MarkerResource.OverlayType.NONE);
 
+        ResourceLocation resource = new ResourceLocation(String.format("textures/entity/villager/markers/%s.png", this.careerName));
 
-        //if (marker == null) {
-            ResourceLocation resource = new ResourceLocation(String.format("textures/entity/villager/markers/%s.png", this.careerName));
-            marker = new MarkerResource(resource, overlayType, this.level);
-        //}
+        boolean found = true;
+
+        try {
+            Minecraft.getMinecraft().getResourceManager().getAllResources(resource);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            found = false;
+        }
+
+        marker = new MarkerResource(found ? resource : DEFAULT_ICON, overlayType, this.level);
 
         resourceCache.put(resourceKey, marker);
         return marker;
